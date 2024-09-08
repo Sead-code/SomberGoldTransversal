a231128b1d4ad1417249170bad90f82f864c3a23
Sead-code <seadahmesula847@gmail.com>
2024-09-08 02:05:12 +0000

    Welcome

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,7 +4,9 @@
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    System.out.println("Welcome to Java") ;
+    System.out.println("Welcome to Computer Science") ;
+    System.out.println("Programming is fun") ;
   }
 
   // @Test
